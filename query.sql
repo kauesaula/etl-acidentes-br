79b06f0888a51f86df2532b03ace56775f6858f9
Kauê Saula <kauecordeiro01@gmail.com>
2026-03-11 18:25:48 -0300

    feat: adiciona query sql para analise de acidentes por dia

--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+-- O objetivo desta query é identificar a volumetria diária de acidentes rodoviários em SP utilizando a base limpa da Camada Prata para suporte à tomada de decisão
 SELECT 
     data_inversa, 
     COUNT(*) as total_acidentes
